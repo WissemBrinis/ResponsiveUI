d29640eb3a7505f19ea57785312f1ec1dfd94f21
WissemBrinis <wissembenbrinis@gmail.com>
2021-11-25 22:34:30 +0100

    Header Finished

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-    return (
-        <div>
-            Header
+  return (
+    <div className="header section__padding">
+      <div className="header-content">
+        <h1 className="gradient__text">
+          Responsive design by Wissem Ben Brinis
+        </h1>
+        <p>Im just building this Responsive Design to get better and better </p>
+        <div className="header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
         </div>
-    )
-}
+        <div className="header-content__people">
+          <img src={people} alt="" />
+          <p>1600 people requested acces visit in the last 24hours</p>
+        </div>
+      </div>
+      <div className="header-image">
+        <img src={ai} alt=" " />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
