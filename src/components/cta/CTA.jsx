5d04ce0db0a3e4f29662471f1d4d50d587282386
WissemBrinis <wissembenbrinis@gmail.com>
2021-11-26 13:10:01 +0100

    click to action finished

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import './CTA.css'
+import React from "react";
+import "./CTA.css";
 
 const CTA = () => {
-    return (
-        <div>
-            CTA
-        </div>
-    )
-}
+  return (
+    <div className="cta section__margin">
+      <div className="cta-content">
+        <p>Request early access</p>
+        <h3>Regiter today & start exploring</h3>
+      </div>
+      <div className="cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default CTA
+export default CTA;
